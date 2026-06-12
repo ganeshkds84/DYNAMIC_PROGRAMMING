3f49aefb3edbd627c0cc8361c937c8d52dea3417
GANESH <ganeshk.ds84@gmail.com>
2026-06-12 23:01:19 +0530

    Find the longest palindromic subsequence length in given string

---
+++ lps.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalinSubseq(self, s):
+
+        n=len(s)
+
+        dp=[[0 for _ in range(n)] for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i]=1
+
+        for i in range(n-1,-1,-1):
+            for j in range(i+1,n):
+                if s[i]==s[j]:
+                    dp[i][j]=2+dp[i+1][j-1]
+                else:
+                    dp[i][j]=max(dp[i+1][j],dp[i][j-1])
+
+        return dp[0][n-1]
+     
+if __name__=='__main__':
+    s=input('Enter string:')
+    Ashu=Solution()
+    print(Ashu.longestPalinSubseq(s))
